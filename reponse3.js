a4ff18a7c0626f0bd01dc518eb71efd65c0c4e41
nour-imene <101825874+nour-imene@users.noreply.github.com>
2022-03-21 23:04:44 +0200

    Create reponse3.js

---
+++ reponse3.js
@@ -0,0 +1,5 @@
+*/
+Cache-Control: no-cacheest généralement utilisé dans un en-tête de requête pour forcer la validation de la ressource dans les proxys intermédiaires.
+Si le client n'envoie pas cette requête au serveur, les proxys intermédiaires renverront une copie du contenu s'il est frais .
+ordonne à ces mandataires de revalider la copie même si elle est récente.
+*/
